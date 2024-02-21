527b11ced26ab8a71f1fc78fc16d122ca303491f
Mathieu Hinderyckx <mathieu.hinderyckx@gmail.com>
2024-02-21 12:38:31 +0100

    asdf

--- config/api_router.py
+++ config/api_router.py
@@ -1,23 +1,14 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from balfolk_music.users.api.views import UserViewSet
-# from balfolk_music.events.api.views import FestivalViewSet, CourseViewSet, BallViewSet
-from balfolk_music.events.api.views import CalendarEventViewSet
-from balfolk_music.music.api.views import PlaylistViewSet, SongSerializer, SongViewSet
+from balfolk_music.music.api.views import PlaylistViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
 else:
     router = SimpleRouter()
 
-# router.register("users", UserViewSet)
-# router.register('festivals', FestivalViewSet, basename='festival')
-# router.register('courses', CourseViewSet, basename='course')
-# router.register('balls', BallViewSet, basename='ball')
-# router.register('calendar_events', CalendarEventViewSet, basename='calendar_event')
 router.register('playlists', PlaylistViewSet, basename='playlist')
-# router.register('songs', SongViewSet, basename='song')
 
 
 app_name = "api"
