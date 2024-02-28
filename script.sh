6ed1f79649475133d486a2688f4cae9a9164984f
Mathieu Hinderyckx <mathieu.hinderyckx@gmail.com>
2024-02-28 10:48:42 +0100

    cleanup

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@ TAG=${GITHUB_REF_NAME}
 echo "TAG: " $TAG
 echo "-------------"
 cd server_config/helm
-sed "s/{IMAGE_TAG}/$TAG/g" balfolk_music/values.tmpl.yaml > balfolk_music/values.yaml
+sed "s/{IMAGE_TAG}/$TAG/g" webapp1/values.tmpl.yaml > webapp1/values.yaml
 mkdir rendered
 mkdir rendered/$TAG
-cp -r balfolk_music/. rendered/$TAG/
+cp -r webapp1/. rendered/$TAG/
 rm rendered/$TAG/values.tmpl.yaml
-rm balfolk_music/values.yaml
+rm webapp1/values.yaml
 
 ls rendered/$TAG
