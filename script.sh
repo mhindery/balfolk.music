2d4387f16a6c95fd342a958b6b71288ab2e49fde
Mathieu Hinderyckx <mathieu.hinderyckx@gmail.com>
2024-02-28 10:24:43 +0100

    actions update

--- script.sh
+++ script.sh
@@ -1,6 +1,10 @@
+TAG=${GITHUB_REF_NAME}
+echo "TAG: " $TAG
+echo "-------------"
 cd server_config/helm
-sed 's/{IMAGE_TAG}/1.0.0/g' balfolk_music/values.tmpl.yaml > balfolk_music/values.yaml
-mkdir rendered/1.0.0
-cp -r balfolk_music/ rendered/1.0.0/
-rm rendered/1.0.0/values.tmpl.yaml
+sed "s/{IMAGE_TAG}/$TAG/g" balfolk_music/values.tmpl.yaml > balfolk_music/values.yaml
+mkdir rendered
+mkdir rendered/$TAG
+cp -r balfolk_music/ rendered/$TAG/
+rm rendered/$TAG/values.tmpl.yaml
 rm balfolk_music/values.yaml
